8796cd2b563988a3cd5449f7d85ceb807546af64
Mohamed.com <m.ahmed.azooz@gmail.com>
2022-06-25 10:12:08 +0200

    Navigate to shopping Cart From Home Screen

--- Shopify/View/Base/BaseNavBar.swift
+++ Shopify/View/Base/BaseNavBar.swift
@@ -21,9 +21,9 @@ class BaseNavBar : UINavigationController
         
         // Set VC(s)
 //        let vc = CheckoutVC.init(nibName: "CheckoutVC", bundle: nil)
-        let vc = ProductDetailsVC.init(nibName: "ProductDetailsVC", bundle: nil)
+//        let vc = ProductDetailsVC.init(nibName: "ProductDetailsVC", bundle: nil)
 //        let vc = LoginRegisterVC.init(nibName: "LoginRegisterVC", bundle: nil)
-        //let vc = BaseTabBar()
+        let vc = BaseTabBar()
         self.viewControllers = [vc]
         
         
