a8f96cb73427d4e56552bced7cb230b6c1d015ef
nadertawah <nadersaidtawah@gmail.com>
2022-07-01 13:56:53 +0200

    1- Added editObj function to CoreData 2-Refactored shoppingCartVm and shoppingCartVC

---
+++ Shopify/Model/CoreData Models/ShoppingCart Product/CartProducts+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CartProducts+CoreDataClass.swift
+//  
+//
+//  Created by Nader Said on 01/07/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CartProducts)
+public class CartProducts: ProductCoreData {
+
+}
